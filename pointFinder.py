66d8c0cf84bc57fc65af2bbf98b88cd3dba1a7d6
fullbloodedengineer <kevin.p.rankin@gmail.com>
2018-12-12 22:39:48 -0500

    Update pointFinder.py

--- pointFinder.py
+++ pointFinder.py
@@ -2,17 +2,19 @@
 import numpy
 import random
 from scipy.spatial import KDTree
-import timeit,cProfile
-import functools
+import time
 
 class Tree:
+    '''Create a KDTree from a defined set of points, KDTree is not designed to be
+        updated. We build the tree once and can quickly find items. 
+    '''
     def __init__(self,pts):
         self.a = numpy.array(pts)
         self.tree = KDTree(self.a, leafsize=10)#smaller leaf size takes longer to build but faster to search
 
     def find_pts(self,test_pt,search_radius):
         distances, ndx = self.tree.query([test_pt], k=5, distance_upper_bound=search_radius)
-        #print distances[0] #We only tested one point so its a single array
+        #print distances[0]
         #print ndx[0]
         index_list = [ndx[0][i] for i,d in enumerate(distances[0]) if d != numpy.inf]
         return index_list
@@ -21,8 +23,6 @@ class Tree:
         pt = create_random_point()
         indexes = tree.find_pts(pt,search_radius)
         print "Found %s within range" %(len(indexes))
-        #for index in indexes:
-        #    print self.a[index]
               
 def create_points(count):
     data = []
@@ -36,4 +36,6 @@ def create_random_point():
 if __name__ == "__main__":
     pts = create_points(10**6)
     tree = Tree(pts)
-    for i in xrange(100): tree.findRandom(100)
+    sT = time.time()
+    for i in xrange(1000000): tree.findRandom(100)
+    print "Completed in",time.time()-sT
